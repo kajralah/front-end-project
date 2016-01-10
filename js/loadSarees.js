47fa298be0e1d91ef2e2356941187962e2ae390d
kajralah <kajralah@gmail.com>
2016-01-10 20:20:15 +0200

    Now we can get all json object in js with jquery :)

--- js/loadSarees.js
+++ js/loadSarees.js
@@ -1,5 +1,14 @@
-function getSarees(){
-	$.getJSON("product_json/sarees.json",function(data){
-		$("test").append(data)
-	})
+function default_product(title,img){
+	var result="";
+	result += "<div><figure><img src="+"'";
+	result += img+"'"+" id=\"img\"/><figcaption><p>";
+	result += title+"</p></figcaption></figure></div>";
+	return result;
 }
+
+function getData(){
+   $.getJSON('product_json/sarees.json',{}, function(data) {
+        $("#test").append(default_product(data["bollywood"][0]["title"],data["bollywood"][0]["img_src"]));
+      });
+}
+
